b5097058e4f91932ee78fb942e682ce192c68991
GabeButler01 <89435460+GabeButler01@users.noreply.github.com>
2022-12-07 00:03:33 -0700

    Game progress

--- wheel.js
+++ wheel.js
@@ -1,50 +1,161 @@
-let theWheel = new Winwheel({
-    'outerRadius'     : 212,        // Set outer radius so wheel fits inside the background.
-    'innerRadius'     : 75,         // Make wheel hollow so segments don't go all way to center.
-    'textFontSize'    : 24,         // Set default font size for the segments.
-    'textOrientation' : 'vertical', // Make text vertial so goes down from the outside of wheel.
-    'textAlignment'   : 'outer',    // Align text to outside of wheel.
-    'numSegments'     : 24,         // Specify number of segments.
-    'segments'        :             // Define segments including colour and text.
-    [                               // font size and test colour overridden on backrupt segments.
-       {'fillStyle' : '#ee1c24', 'text' : '300'},
-       {'fillStyle' : '#3cb878', 'text' : '450'},
-       {'fillStyle' : '#f6989d', 'text' : '600'},
-       {'fillStyle' : '#00aef0', 'text' : '750'},
-       {'fillStyle' : '#f26522', 'text' : '500'},
-       {'fillStyle' : '#000000', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#ffffff'},
-       {'fillStyle' : '#e70697', 'text' : '3000'},
-       {'fillStyle' : '#fff200', 'text' : '600'},
-       {'fillStyle' : '#f6989d', 'text' : '700'},
-       {'fillStyle' : '#ee1c24', 'text' : '350'},
-       {'fillStyle' : '#3cb878', 'text' : '500'},
-       {'fillStyle' : '#f26522', 'text' : '800'},
-       {'fillStyle' : '#a186be', 'text' : '300'},
-       {'fillStyle' : '#fff200', 'text' : '400'},
-       {'fillStyle' : '#00aef0', 'text' : '650'},
-       {'fillStyle' : '#ee1c24', 'text' : '1000'},
-       {'fillStyle' : '#f6989d', 'text' : '500'},
-       {'fillStyle' : '#f26522', 'text' : '400'},
-       {'fillStyle' : '#3cb878', 'text' : '900'},
-       {'fillStyle' : '#000000', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#ffffff'},
-       {'fillStyle' : '#a186be', 'text' : '600'},
-       {'fillStyle' : '#fff200', 'text' : '700'},
-       {'fillStyle' : '#00aef0', 'text' : '800'},
-       {'fillStyle' : '#ffffff', 'text' : 'LOOSE TURN', 'textFontSize' : 12}
-    ],
-    'animation' :           // Specify the animation to use.
-    {
-        'type'     : 'spinToStop',
-        'duration' : 10,    // Duration in seconds.
-        'spins'    : 3,     // Default number of complete spins.
-        'callbackFinished' : alertPrize,
-        'callbackSound'    : playSound,   // Function to call when the tick sound is to be triggered.
-        'soundTrigger'     : 'pin'        // Specify pins are to trigger the sound, the other option is 'segment'.
-    },
-    'pins' :				// Turn pins on.
-    {
-        'number'     : 24,
-        'fillStyle'  : 'silver',
-        'outerRadius': 4,
+(function() {
+    const wheel = document.querySelector('.wheel');
+    const startButton = document.querySelector('.spin');
+
+    const body = document.querySelector('body');
+    const nav = document.querySelector('nav');
+
+    let deg = 0;
+    let zoneSize = 45; // deg
+
+      // Counter clockwise
+    const symbolSegments = {
+        1: "Close",
+        2: "Advice",
+        3: "RPS",
+        4: "Video",
+        5: "Koala",
+        6: "Unicorn",
+        7: "Dragon",
+        8: "Snowman",
     }
-});
+
+    const handleWin = (actualDeg) => {
+        const winningSymbolNr = Math.ceil(actualDeg / zoneSize);
+
+
+/*         function reveal(){
+            var audio = new Audio('scream.mp3');
+            audio.play();
+            var hampster = document.getElementById("hide");
+            hampster.style.visibility = 'visible';
+        }
+
+        reveal(); */
+        if(winningSymbolNr == 1){
+            const sleep = async (milliseconds) => {
+                await new Promise(resolve => {
+                    return setTimeout(resolve, milliseconds)
+                });
+            };
+            
+            const close = async () => {
+                await sleep(500);
+                window.opener=null;
+                window.open('','_self');
+                window.close();
+                window.history.go(-1);
+                $(document.body).hide()
+            }
+
+            close();
+        }
+        if(winningSymbolNr == 2){
+            const endpoint = 'https://api.adviceslip.com/advice';
+
+            async function getQuote(){
+                let text = await fetch(endpoint);
+                let response = await text.text();
+
+                let json_response = JSON.parse(response);
+                alert(json_response.slip.advice);
+            }
+
+            getQuote();
+        }
+        if(winningSymbolNr == 3){
+            var options =['Rock', 'Paper', 'Scissors'];
+            var choice = options[Math.floor(Math.random()*options.length)];
+
+            var answer = prompt("Rock, Paper, or Scissors?");
+
+            if(answer == 'Rock'){
+                if(choice == 'Rock'){
+                    alert("We tied :|");
+                }
+                else if(choice == 'Paper'){
+                    alert("I win :D");
+                }
+                else{
+                    alert("You win >:(");
+                }
+            }
+
+            else if(answer == 'Paper'){
+                if(choice == 'Rock'){
+                    alert("You win >:(");
+                }
+                else if(choice == 'Paper'){
+                    alert("We tied :|");
+                }
+                else{
+                    alert("I win :D");
+                }
+            }
+
+            else if(answer == 'Scissors'){
+                if(choice == 'Rock'){
+                    alert("I win :D");
+                }
+                else if(choice == 'Paper'){
+                    alert("You win >:(");
+                }
+                else{
+                    alert("We tied :|");
+                }
+            }
+
+            else{
+                alert("That wasn't an option...so I guess I win :/");
+            }
+            
+        }
+        if(winningSymbolNr == 4){
+            window.open("https://youtu.be/9YyTZwFEdGY", "_blank");
+        }
+        if(winningSymbolNr == 5){
+            alert("hi");
+        }
+        if(winningSymbolNr == 6){
+            alert("hi");
+        }
+        if(winningSymbolNr == 7){
+            alert("hi");
+        }
+        if(winningSymbolNr == 8){
+            alert("hi");
+        }
+    }
+  
+    startButton.addEventListener('click', () => {
+
+        // Disable button during spin
+      startButton.style.pointerEvents = 'none';
+      // Calculate a new rotation between 5000 and 10 000
+      deg = Math.floor(5000 + Math.random() * 5000);
+      // Set the transition on the wheel
+      wheel.style.transition = 'all 1s ease-out';
+      // Rotate the wheel
+      wheel.style.transform = `rotate(${deg}deg)`;
+      // Apply the blur
+      wheel.classList.add('blur');
+    });
+  
+    wheel.addEventListener('transitionend', () => {
+      // Remove blur
+      wheel.classList.remove('blur');
+      // Enable button when spin is over
+      startButton.style.pointerEvents = 'auto';
+      // Need to set transition to none as we want to rotate instantly
+      wheel.style.transition = 'none';
+      // Calculate degree on a 360 degree basis to get the "natural" real rotation
+      // Important because we want to start the next spin from that one
+      // Use modulus to get the rest value from 360
+      const actualDeg = deg % 360;
+      // Set the real rotation instantly without animation
+      wheel.style.transform = `rotate(${actualDeg}deg)`;
+      // Calculate and display the winning symbol
+      handleWin(actualDeg);
+
+    });
+  })();
